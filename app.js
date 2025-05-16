79c80f2ff8b61df933ba209d4e2feb3a0b6702c2
Eni Bondarenko <bondarenkoenny@gmail.com>
2025-05-16 11:34:52 +0200

    create header menu

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-// const heading = React.createElement("h1", {id: "heading"}, "Hello world from React");
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import css from './src/style.css'
+
+const App = () => {
+    return(
+        <>
+        <div className='menuContainer'>
+            <ul className='menu'>
+            <a href=""><li>Apartments</li></a>
+            <a href=""><li>Hotels</li></a>
+            <a href=""><li>Lands</li></a>
+            <a href=""><li>Tours</li></a>
+            <a href=""><li>WebPages</li></a>
+            <a href=""><li>Charity</li></a>
+            </ul>
+        </div>
+        </>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
 
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id:"child"}, [
-        React.createElement("h1", {}, "i'm html tag"),
-        React.createElement("h2", {}, "i'm html tag 2"),
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "i'm htm tag"),
-        React.createElement("h2", {}, "i'm html tag") 
-    ])
-])
+
+
+
+
+
+
+
+
+
+
+
+
+// const parent = React.createElement("div", {id: "parent"}, [
+//     React.createElement("div", {id:"child"}, [
+//         React.createElement("h1", {}, "i'm html tag"),
+//         React.createElement("h2", {}, "i'm html tag 2"),
+//     ]),
+//     React.createElement("div", {id: "child2"}, [
+//         React.createElement("h1", {}, "i'm htm tag"),
+//         React.createElement("h2", {}, "i'm html tag") 
+//     ])
+// ])
---
+++ dist/react-basics.7c0ccee6.js
@@ -0,0 +1,13 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading); // const parent = React.createElement("div", {id: "parent"}, [
+ //     React.createElement("div", {id:"child"}, [
+ //         React.createElement("h1", {}, "i'm html tag"),
+ //         React.createElement("h2", {}, "i'm html tag 2"),
+ //     ]),
+ //     React.createElement("div", {id: "child2"}, [
+ //         React.createElement("h1", {}, "i'm htm tag"),
+ //         React.createElement("h2", {}, "i'm html tag") 
+ //     ])
+ // ])
+
+//# sourceMappingURL=react-basics.7c0ccee6.js.map
